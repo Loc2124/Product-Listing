b46c2deab3d16474ec1cdb47609e8e7bd69a9419
Loc2124 <nguyendinhloc21122004@gmail.com>
2025-04-11 10:29:38 +0700

    Update index.html with pagination and product display logic

--- obj/Debug/net9.0/ProductApi.AssemblyInfo.cs
+++ obj/Debug/net9.0/ProductApi.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ProductApi")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+68bafeada33a72735576d37473f9ba5af69d477f")]
 [assembly: System.Reflection.AssemblyProductAttribute("ProductApi")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ProductApi")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
